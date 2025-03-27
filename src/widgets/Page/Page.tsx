5e684c088fd7e65e5554cd809cdf80b920116b52
gwizxs <gwizxs@gmail.com>
2025-03-27 18:55:45 +0300

    add infinitive scroll

--- src/widgets/Page/Page.tsx
+++ src/widgets/Page/Page.tsx
@@ -1,17 +1,37 @@
-import classNames from "shared/library/classNames/classNames";
-import s from './Page.module.scss'
-import { ReactNode } from "react";
-
+import classNames from 'shared/library/classNames/classNames';
+import {
+    MutableRefObject, ReactNode, useRef,
+} from 'react';
+import s from './Page.module.scss';
+import { useInfiniteScroll } from 'shared/library/hooks/useInfiniteScroll';
 
 interface PageProps {
-    className?: string;
-    children: ReactNode
+    className?: string
+    children?: ReactNode;
+    onScrollEnd?: () => void
 }
 
-export const Page = ({className, children}: PageProps) => {
+export const PAGE_ID = 'PAGE_ID';
+
+export const Page = (props: PageProps) => {
+    const { className, children, onScrollEnd } = props;
+    const wrapperRef = useRef() as MutableRefObject<HTMLDivElement>;
+    const triggerRef = useRef() as MutableRefObject<HTMLDivElement>;
+
+    useInfiniteScroll({
+        triggerRef,
+        wrapperRef,
+        callback: onScrollEnd,
+    });
+
     return (
-        <main className={classNames(s.Page, {}, [className])}>
+        <main
+            ref={wrapperRef}
+            className={classNames(s.Page, {}, [className])}
+            id={PAGE_ID}
+        >
             {children}
+            {onScrollEnd ? <div className={s.trigger} ref={triggerRef} /> : null}
         </main>
     );
 };
